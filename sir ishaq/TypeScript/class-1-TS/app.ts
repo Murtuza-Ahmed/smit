a8223d806ba6afdf814e9b20af46fe8b6db8701a
murtuza_ahmed <murtuza.programmer@gmail.com>
2024-05-28 00:44:25 +0500

    class-ts-stop

--- sir ishaq/TypeScript/class-1-TS/app.ts
+++ sir ishaq/TypeScript/class-1-TS/app.ts
@@ -9,3 +9,50 @@ function add(num1: number, num2: number) {
 button.addEventListener("click", () => {
     console.log(add(+input1.value, +input2.value));
 });
+
+
+/* 
+1| NUMBER 5
+2| Decimal Number 5.5
+3| (-) ME KOI VALUE AI ISKI TYPE BHI NUMBER HOGI
+4| STRING
+5| BOOLEAN TRUE/FALSE
+*/
+
+
+let person = "Sheroz";
+person = "Sheikh";
+
+let num: number;
+num = 5
+
+let personName: string;
+personName = "Sheikh";
+
+let bool: boolean;
+bool = false;
+bool = true;
+
+// ARRAY KI TYPE TYPE OF ARRAY
+
+// const nameArr = ["sheroz", "raza", "ahmed", "sheikh", "murtuza"];
+
+let nameArr: string[] = [];
+nameArr.push("sheroz");
+
+let numberArr: number[] = [];
+numberArr.push(5);
+
+
+// OBJECT KI TYPE TYPE OF OBJECT
+
+const personObj: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    email?: string;
+} = {
+    name: "sheroz",
+    age: 25,
+    hobbies: ["football", "basketball"]
+}
