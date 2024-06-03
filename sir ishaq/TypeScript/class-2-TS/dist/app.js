942ad99ebeb57c7c22fa11178f7c40f1ffce4640
murtuza_ahmed <murtuza.programmer@gmail.com>
2024-06-03 09:58:58 +0500

    class-end

---
+++ sir ishaq/TypeScript/class-2-TS/dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const button = document.querySelector("button"); //    ! Confirm Karta He
+button.addEventListener("click", () => {
+    console.log("clicked");
+});
+//# sourceMappingURL=app.js.map
---
+++ sir ishaq/TypeScript/class-2-TS/src/app.ts
@@ -0,0 +1,5 @@
+const button = document.querySelector("button")!; //    ! Confirm Karta He
+
+button.addEventListener("click", () => {
+    console.log("clicked");
+});
