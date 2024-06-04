9cd27f11bdc504bf76a00db19298979bda7d6cf9
murtuza_ahmed <murtuza.programmer@gmail.com>
2024-06-04 09:54:04 +0500

    typescript class-2 paractice

--- sir ishaq/TypeScript/class-2-TS/dist/app.js
+++ sir ishaq/TypeScript/class-2-TS/dist/app.js
@@ -1,6 +1,14 @@
 "use strict";
 const button = document.querySelector("button"); //    ! Confirm Karta He
-button.addEventListener("click", () => {
-    console.log("clicked");
-});
+// button.addEventListener("click", () => {
+//     console.log("clicked");
+// });
+function log(data) {
+    console.log("clicked", data);
+}
+log("Hello");
+function login(data) {
+    console.log("DATA", data);
+}
+button.addEventListener("click", login.bind(null, "Raza")); // .bind FIRST null | this AND Argument
 //# sourceMappingURL=app.js.map
--- sir ishaq/TypeScript/class-2-TS/src/app.ts
+++ sir ishaq/TypeScript/class-2-TS/src/app.ts
@@ -1,5 +1,15 @@
 const button = document.querySelector("button")!; //    ! Confirm Karta He
 
-button.addEventListener("click", () => {
-    console.log("clicked");
-});
+// button.addEventListener("click", () => {
+//     console.log("clicked");
+// });
+
+function log(data: any) {
+    console.log("clicked", data)
+}
+log("Hello");
+
+function login(data: string) {
+    console.log("DATA", data)
+}
+button.addEventListener("click", login.bind(null, "Raza")); // .bind FIRST null | this AND Argument
