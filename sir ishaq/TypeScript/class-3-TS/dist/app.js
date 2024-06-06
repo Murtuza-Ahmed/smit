743c721d0207f502f2b98b41b505f3ad6a19f3f8
murtuza_ahmed <murtuza.programmer@gmail.com>
2024-06-06 18:52:57 +0500

    class-3 typescript

---
+++ sir ishaq/TypeScript/class-3-TS/dist/app.js
@@ -0,0 +1,13 @@
+"use strict";
+/**
+ * interface me (=) ka sing nai hota he
+ * (TYPE) or (INTERFACE) dono ek hi he ek chota sa farq he
+ * (type) classes par kam nai karti
+ * (interface) classes par bhi kam karti hn
+ */
+// INTERFACE
+let interfaceExm;
+interfaceExm = {
+    name: "sheroz",
+};
+//# sourceMappingURL=app.js.map
---
+++ sir ishaq/TypeScript/class-3-TS/src/app.ts
@@ -0,0 +1,17 @@
+/**
+ * interface me (=) ka sing nai hota he 
+ * (TYPE) or (INTERFACE) dono ek hi he ek chota sa farq he
+ * (type) classes par kam nai karti
+ * (interface) classes par bhi kam karti hn
+ */
+// INTERFACE
+
+interface Person {
+    name: string;
+}
+
+let interfaceExm: Person;
+interfaceExm = {
+    name: "sheroz",
+}
+
