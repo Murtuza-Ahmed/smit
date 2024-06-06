904cc81480cd5c4c5dbbda8b2f065d5fce041cff
murtuza_ahmed <murtuza.programmer@gmail.com>
2024-06-06 09:23:46 +0500

    class typescript classes

--- sir ishaq/TypeScript/class-2-TS/dist/app.js
+++ sir ishaq/TypeScript/class-2-TS/dist/app.js
@@ -10,5 +10,61 @@ log("Hello");
 function login(data) {
     console.log("DATA", data);
 }
-button.addEventListener("click", login.bind(null, "Raza")); // .bind FIRST null | this AND Argument
+button.addEventListener("click", login.bind(this, "Raza")); // .bind FIRST null | this AND Argument
+// CLASSES
+class Student {
+    // constructor(n: string, r: number) { // LONG CODE
+    //     this.name = n;
+    //     this.rollNo = r;
+    // };
+    constructor(name, rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+        // name: string;
+        // rollNo: number;
+        this.skills = []; // MERE ARRAY ME KOI PROBLEM CREATE NA HO IS LYE ISE (PRIVATE) RAKHTE HN BUT PHIR ISSE ACCESS NAHI KAR SAKTE IS KYLYE (GETTER OR SUTTER) KA IS TEMAL KARTE HN YA PHIR FUNCTION KA ISTEMAL KARTE HN
+    } // SHORT HAND CODE
+    addSkill(skill) {
+        this.skills.push(skill);
+    }
+    ;
+    getSkills() {
+        return this.skills;
+    }
+    ;
+}
+;
+const student1 = new Student("Sheroz", 27);
+student1.addSkill("javascript");
+// student1.skills[2] = "TS";  // Problem empty Array (SKILLS IS PRIVATE PROPERTY NOT ACCESSEBL)
+student1.getSkills(); // PRIVATE GET SKILLS GET
+const student2 = new Student("Raza", 30);
+student2.addSkill("html");
+console.log(student1, student2);
+// CLASSES INHERITANCE
+class StudentSmit {
+    constructor(n, r) {
+        this.skills = []; // MERE ARRAY ME KOI PROBLEM CREATE NA HO IS LYE ISE (PRIVATE) RAKHTE HN BUT PHIR ISSE ACCESS NAHI KAR SAKTE IS KYLYE (GETTER OR SUTTER) KA IS TEMAL KARTE HN YA PHIR FUNCTION KA ISTEMAL KARTE HN
+        this.name = n;
+        this.rollNo = r;
+    }
+    ;
+    // constructor(public name: string, public rollNo: number) { } // SHORT HAND CODE
+    addSkill(skill) {
+        this.skills.push(skill);
+    }
+    ;
+    getSkills() {
+        return this.skills;
+    }
+    ;
+}
+;
+const studentSit = new StudentSmit("Sheroz", 27);
+studentSit.addSkill("javascript");
+// student1.skills[2] = "TS";  // Problem empty Array (SKILLS IS PRIVATE PROPERTY NOT ACCESSEBL)
+studentSit.getSkills(); // PRIVATE GET SKILLS GET
+const studentSt = new StudentSmit("Raza", 30);
+student2.addSkill("html");
+console.log(studentSit, studentSt);
 //# sourceMappingURL=app.js.map
--- sir ishaq/TypeScript/class-2-TS/src/app.ts
+++ sir ishaq/TypeScript/class-2-TS/src/app.ts
@@ -12,4 +12,68 @@ log("Hello");
 function login(data: string) {
     console.log("DATA", data)
 }
-button.addEventListener("click", login.bind(null, "Raza")); // .bind FIRST null | this AND Argument
+button.addEventListener("click", login.bind(this, "Raza")); // .bind FIRST null | this AND Argument
+
+
+// CLASSES
+
+class Student {
+    // name: string;
+    // rollNo: number;
+    private skills: string[] = [];  // MERE ARRAY ME KOI PROBLEM CREATE NA HO IS LYE ISE (PRIVATE) RAKHTE HN BUT PHIR ISSE ACCESS NAHI KAR SAKTE IS KYLYE (GETTER OR SUTTER) KA IS TEMAL KARTE HN YA PHIR FUNCTION KA ISTEMAL KARTE HN
+
+    // constructor(n: string, r: number) { // LONG CODE
+    //     this.name = n;
+    //     this.rollNo = r;
+    // };
+    constructor(public name: string, public rollNo: number) { } // SHORT HAND CODE
+
+    addSkill(skill: string) {
+        this.skills.push(skill);
+    };
+    getSkills() {   // IS FUNCTION KI HELP SE PRIVATE SKILLS KO GET KAR SAKTE HN
+        return this.skills;
+    };
+};
+
+const student1 = new Student("Sheroz", 27);
+student1.addSkill("javascript");
+// student1.skills[2] = "TS";  // Problem empty Array (SKILLS IS PRIVATE PROPERTY NOT ACCESSEBL)
+student1.getSkills();   // PRIVATE GET SKILLS GET
+const student2 = new Student("Raza", 30);
+student2.addSkill("html");
+console.log(student1, student2);
+
+
+
+
+// CLASSES INHERITANCE
+
+class StudentSmit {
+    name: string;
+    rollNo: number;
+    private skills: string[] = [];  // MERE ARRAY ME KOI PROBLEM CREATE NA HO IS LYE ISE (PRIVATE) RAKHTE HN BUT PHIR ISSE ACCESS NAHI KAR SAKTE IS KYLYE (GETTER OR SUTTER) KA IS TEMAL KARTE HN YA PHIR FUNCTION KA ISTEMAL KARTE HN
+
+    constructor(n: string, r: number) { // LONG CODE
+        this.name = n;
+        this.rollNo = r;
+    };
+    // constructor(public name: string, public rollNo: number) { } // SHORT HAND CODE
+
+    addSkill(skill: string) {
+        this.skills.push(skill);
+    };
+    getSkills() {   // IS FUNCTION KI HELP SE PRIVATE SKILLS KO GET KAR SAKTE HN
+        return this.skills;
+    };
+};
+
+
+
+const studentSit = new StudentSmit("Sheroz", 27);
+studentSit.addSkill("javascript");
+// student1.skills[2] = "TS";  // Problem empty Array (SKILLS IS PRIVATE PROPERTY NOT ACCESSEBL)
+studentSit.getSkills();   // PRIVATE GET SKILLS GET
+const studentSt = new StudentSmit("Raza", 30);
+student2.addSkill("html");
+console.log(studentSit, studentSt);
