f8ffd86b4da127b016583e7973fcbb2aa240a367
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-06-19 00:39:17 +0500

    class-1 end

--- sir ishaq/REACT-JS/class-1-reactjs/src/components/item/Items.jsx
+++ sir ishaq/REACT-JS/class-1-reactjs/src/components/item/Items.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 Items.propTypes = {
   title: PropTypes.string.isRequired,
 };
 
-function Items({ title }) {
+function Items(props) {
   //   const data = "My React App Items";
+  const [title, setTitle] = useState(props.title);
+  //   [vatToUse,functionToUpdate]
+
+  const changeIt = () => {
+    setTitle("update");
+    // console.log(title);
+  };
   return (
     <>
       {/* <li>{data}</li> */}
       <li>{title}</li>
+      <button onClick={changeIt}>Change</button>
     </>
   );
 }
