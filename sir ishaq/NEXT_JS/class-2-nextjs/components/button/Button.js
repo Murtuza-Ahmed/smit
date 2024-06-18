6a96de3be618a6119d27ca17a5e4ae84598c4433
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-06-18 16:08:47 +0500

    error component button

---
+++ sir ishaq/NEXT_JS/class-2-nextjs/components/button/Button.js
@@ -0,0 +1,9 @@
+export default function Button(children) {
+    // console.log(props)
+    return (
+        <>
+            {/* <button>{text}</button> */}
+            {/* <button>{children}</button> */}
+        </>
+    )
+}
--- sir ishaq/NEXT_JS/class-2-nextjs/pages/index.js
+++ sir ishaq/NEXT_JS/class-2-nextjs/pages/index.js
@@ -1,6 +1,12 @@
+import Button from "@/components/button/Button";
+import Greet from "@/components/greet/Greet";
+
 export default function Home() {
   return (
     <>
+      <Greet />
+      {/* <Button text="save" /> */}
+      <Button>Cancel</Button>
       <h1>Sheroz</h1>
     </>
   );
