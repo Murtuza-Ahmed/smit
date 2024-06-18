524fa4df35e9503a4c6cd7094b0c24906dccac0d
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-06-18 14:45:27 +0500

    class-1 end and nextjs paractice pages

---
+++ sir ishaq/NEXT_JS/class-1-nextjs/pages/sheroz.js
@@ -0,0 +1,8 @@
+export default function Sheroz() {
+    return (
+        <>
+            <h1>Hello</h1>
+            <p>I am Sheroz</p>
+        </>
+    )
+}
---
+++ sir ishaq/NEXT_JS/class-1-nextjs/pages/users/raza.js
@@ -0,0 +1,8 @@
+export default function Raza() {
+    return (
+        <>
+            <h3>Raza</h3>
+            <p>I, am Raza</p>
+        </>
+    )
+}
