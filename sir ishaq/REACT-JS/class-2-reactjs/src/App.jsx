f4be3a61665d7f10c707cb1448f3908572bd172c
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-06-19 20:39:57 +0500

    conditnal class style

--- sir ishaq/REACT-JS/class-2-reactjs/src/App.jsx
+++ sir ishaq/REACT-JS/class-2-reactjs/src/App.jsx
@@ -2,12 +2,29 @@ import "./App.css";
 import Lists from "./components/list/Lists";
 
 function App() {
+  const bool = true;
+  const col = true;
+
+  // let cls = "my-class";
+  // if (bool) {
+  //   cls += " ";
+  // }
+
   return (
     <>
-      <div>
+      {/* <div className={bool ? "app" : ""}> */}
+      <div
+        // className={`my-class ${bool ? "app" : "my-app"}`}
+        className={`my-class ${bool && "app"}`}
+        style={{ backgroundColor: col ? "#000" : "#61da" }}
+      >
         My React App
         <Lists />
       </div>
+      
+      {/* <div className={cls}>
+        <Lists />
+      </div> */}
     </>
   );
 }
--- sir ishaq/REACT-JS/class-2-reactjs/src/components/list/Lists.jsx
+++ sir ishaq/REACT-JS/class-2-reactjs/src/components/list/Lists.jsx
@@ -2,12 +2,17 @@ import Items from "../item/Items";
 
 function Lists() {
   const arr = ["item 1", "item 2", "item 3", "item 4", "item 5"];
+
+  const items = arr.map((item, index) => {
+    return <Items key={"no-" + index} title={item} />;
+  });
   return (
     <>
       <ul>
-        {arr.map((item, index) => (
+        {items}
+        {/* {arr.map((item, index) => (
           <Items key={index} title={item} />
-        ))}
+        ))} */}
         {/* <Items title={arr[0]} />
         <Items title={arr[1]} /> */}
       </ul>
