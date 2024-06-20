80dffe0010624c24bbd74f0f9ae70eefb0f7ccfb
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-06-20 19:07:22 +0500

    useEffect read and useEffect cleanUp func

--- sir ishaq/REACT-JS/class-3-reactjs/src/components/login/Login.jsx
+++ sir ishaq/REACT-JS/class-3-reactjs/src/components/login/Login.jsx
@@ -1,5 +1,5 @@
 // import PropTypes from "prop-types";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import AuthContext from "../../context/AuthContext";
 
 // Login.propTypes = {
@@ -7,16 +7,33 @@ import AuthContext from "../../context/AuthContext";
 // };
 
 function Login() {
-  const authCtx = useContext(AuthContext);
-
   const [data, setData] = useState("");
+  const [data1, setData1] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  useEffect(() => {
+    console.log("click");
+    setIsValid(data.length >= 6);
+  }, [data]);
   const onTextHandler = (event) => {
     setData(event.target.value);
+    // validate();
   };
+
+  const data1Change = (event) => {
+    setData1(event.target.value);
+    // validate();
+  };
+  // const validate = () => {
+  //   console.log("VALIDATION");
+  //   // setIsValid(data.length >= 6);
+  // };
+  const authCtx = useContext(AuthContext);
   return (
     <>
       <div>Login Page</div>
       <input type="text" onChange={onTextHandler} value={data} />
+      {isValid ? "inValid" : "Valid"}
+      <input type="text" value={data1} onChange={data1Change} />
       <button onClick={authCtx.onLogin}>Login</button>
     </>
   );
