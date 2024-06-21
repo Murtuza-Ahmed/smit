401f236620305c76d6d7685f34e1e979b1a6b4d2
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-06-22 01:38:04 +0500

    read scss and react router and paractice fetch api

--- sir ishaq/REACT-JS/class-4-reactjs/src/components/home/Home.jsx
+++ sir ishaq/REACT-JS/class-4-reactjs/src/components/home/Home.jsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1", { method: "GET" })
+      .then((response) => {
+        console.log("RESPONSE", response);
+        return response.json();
+      })
+      .then((json) => setData(json));
+  }, []);
+  // const { userId, title, completed } = data;
   return (
     <>
       Home
       <div>
+        UserID: {data.userId} <br />
+        Title: {data.title} <br />
+        Completed: {data.completed}
+      </div>
+      <div>
         <Link to="about">About</Link>
       </div>
-        <Link to="invoices/123">My Invoices</Link>
+      <Link to="invoices/123">My Invoices</Link>
     </>
   );
 };
