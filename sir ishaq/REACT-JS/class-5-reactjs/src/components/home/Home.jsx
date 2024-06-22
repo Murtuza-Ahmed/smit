ddaf23667e27e6887769fb22d5e34782552c81bc
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-06-22 16:01:08 +0500

    home create import soap-buy soap-sell

--- sir ishaq/REACT-JS/class-5-reactjs/src/components/home/Home.jsx
+++ sir ishaq/REACT-JS/class-5-reactjs/src/components/home/Home.jsx
@@ -1,14 +1,15 @@
 import { useSelector, useDispatch } from "react-redux";
+import { SOAP_BUY, SOAP_SELL } from "../../store/action";
 
 const Home = () => {
   const soap = useSelector((state) => state.soap);
   const dispatch = useDispatch();
 
   const sellHandler = () => {
-    dispatch({ type: "SOAP_SELL" });
+    dispatch({ type: SOAP_SELL });
   };
   const buyHandler = () => {
-    dispatch({ type: "SOAP_BUY", payload: 10 });
+    dispatch({ type: SOAP_BUY, payload: 10 });
   };
 
   return (
