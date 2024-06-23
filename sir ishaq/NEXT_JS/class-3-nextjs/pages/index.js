213de8d92a1ecaff955932b856d8c38fb088e2cc
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-06-23 12:48:37 +0500

    next-js state and effect paractice and api data fetch

--- sir ishaq/NEXT_JS/class-3-nextjs/pages/index.js
+++ sir ishaq/NEXT_JS/class-3-nextjs/pages/index.js
@@ -1,16 +1,48 @@
 
-import { useState } from "react";
-import db from '../utils/db.json';
-import Lists from "@/components/listdata/Lists";
+import { useEffect, useState } from "react";
+// import db from '../utils/db.json';
+import Lists from "@/components/listdata/Lists.jsx";
 
-export default function Home() {
+const Home = () => {
 
-  console.log(db);
-  const [arr, setArr] = useState(db.authors);
+  // console.log(db);
+  // const [arr, setArr] = useState(db.authors);
+
+  const [data, setData] = useState(1);
+
+  const [data2, setData2] = useState(1);
+
+  useEffect(() => {
+    console.log("called");
+  }, [data])
+
+  const update = () => {
+    setData((oldData) => {
+      const d = oldData + 1
+      console.log(d)
+      return d
+    });
+    console.log(data)
+  }
+
+  const update2 = () => {
+    setData2((prevState) => {
+      const d = prevState + 1
+      console.log(d)
+      return d
+    })
+  }
 
   return (
     <div>
-      <Lists arr={arr} />
+      DATA: {data} 
+      <button onClick={update}>Add</button>
+      DATA:2 {data2}
+      <button onClick={update2}>Add Data 2</button>
+      {/* <Lists arr={arr} /> */}
     </div>
   )
 }
+
+
+export default Home
