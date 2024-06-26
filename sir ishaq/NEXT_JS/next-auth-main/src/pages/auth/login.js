3112032257f06a34406f045c05ac39d32a335185
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-06-26 21:22:09 +0500

    new setup next-js and read authentication next-js

---
+++ sir ishaq/NEXT_JS/next-auth-main/src/pages/auth/login.js
@@ -0,0 +1,5 @@
+import Form from "@/components/auth/form";
+
+export default function SignIn () {
+    return <Form signin={true} />
+};
---
+++ sir ishaq/NEXT_JS/next-auth-main/src/pages/auth/signup.js
@@ -0,0 +1,5 @@
+import Form from "@/components/auth/form";
+
+export default function SignUp () {
+    return <Form signin={false} />
+};
