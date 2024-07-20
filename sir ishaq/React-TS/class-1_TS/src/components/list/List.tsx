298330c90281dbe850a673bda35e2d88ec23cf01
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-07-20 12:53:50 +0500

    class-1-typescript end

--- sir ishaq/React-TS/class-1_TS/src/components/list/List.tsx
+++ sir ishaq/React-TS/class-1_TS/src/components/list/List.tsx
@@ -1,10 +1,12 @@
 import Item from "../item/Item";
 
 function List({ data }: { data: string[] }) {
+  const item = data.map((item, index) => <Item data={item} key={index} />);
   return (
     <>
       <div>
-        <Item data={data[0]} />
+        {/* <Item data={data[2]} /> */}
+        {item}
       </div>
     </>
   );
