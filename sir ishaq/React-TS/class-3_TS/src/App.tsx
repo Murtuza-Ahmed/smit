a1b798d0cd791c1cab7050fa4832d5edc85e33e5
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-07-21 01:03:34 +0500

    call useeffect

--- sir ishaq/React-TS/class-3_TS/src/App.tsx
+++ sir ishaq/React-TS/class-3_TS/src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AuthContext from "./contexts/AuthContext";
 import AuthHandler from "./components/authHandler/AuthHandler";
 
 function App() {
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem("isloggedin") === "1";
+    setIsAuth(isLoggedIn);
+  }, []);
   const [isAuth, setIsAuth] = useState<boolean>(false);
 
   const ctxValue = {
     isLoggedIn: isAuth,
     setAuthentication: (val: boolean) => {
+      localStorage.setItem("isloggedin", val ? "1" : "0");
       setIsAuth(val);
     },
   };
