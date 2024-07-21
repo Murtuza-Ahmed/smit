07e2aa3045d0c5ba773c7037b4bab35e3c8e93f3
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-07-21 11:59:20 +0500

    component destroy and class-3-TS end

--- sir ishaq/React-TS/class-3_TS/src/components/todo/Todo.tsx
+++ sir ishaq/React-TS/class-3_TS/src/components/todo/Todo.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddForm from "../addForm/AddForm";
 import List from "../list/List";
 import Logout from "../logout/Logout";
 
 function Todo() {
+  useEffect(() => {
+    const isIntervel = setInterval(() => console.log("run"), 1000);
+    return () => {
+      clearInterval(isIntervel);
+    };
+  }, []);
   const [items, setItems] = useState<string[]>([
     "Hello",
     "Welcome",
