19a5593248a896266f42af72c79ec773f68d04ec
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-07-28 11:21:58 +0500

    create router and new file form node js

--- sir ishaq/Node-JS/class-2-NodeJS/app.js
+++ sir ishaq/Node-JS/class-2-NodeJS/app.js
@@ -1,3 +1,13 @@
 const express = require("express");
 const app = express();
+
+app.use((req, res, next) => {   //  AUTH MIDDLEWARE 
+    req.data = "Sheroz";
+    next();
+});
+
+app.use((req, res, next) => {
+    res.send(req.data);
+});
+
 app.listen(3000);
