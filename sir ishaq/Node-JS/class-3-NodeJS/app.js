0444539f7bc45a7e6d5d07310252c273ae7c5728
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-07-29 20:18:55 +0500

    class-3 node MVS express

--- sir ishaq/Node-JS/class-3-NodeJS/app.js
+++ sir ishaq/Node-JS/class-3-NodeJS/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const form = require("./routes/form");
+const todo = require("./routes/todo");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();  //  application run hogi app me
@@ -8,11 +9,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(process.cwd(), "public")));
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 app.use((req, res, next) => {   //  AUTH MIDDLEWARE 
     req.data = "Sheroz";
     next();
 });
 
+app.use("/todo", todo);
 app.use("/form", form);
 
 app.listen(3000);
--- sir ishaq/Node-JS/class-3-NodeJS/routes/form.js
+++ sir ishaq/Node-JS/class-3-NodeJS/routes/form.js
@@ -1,18 +1,14 @@
+const { name } = require("ejs");
 const express = require("express");
 const path = require("path");
 const router = express.Router();
 
 // router.get("/", (req, res) => { //  ROUTER ESA BANE GA /form/
-//     res.send(`
-//         <form action="/form/submit" method="POST">
-//         <input type="text" name="data"/>
-//         <button>Submit</button>
-//         </form>
-//         `);
+//     res.sendFile(path.join(process.cwd(), "views", "form.html"));
 // });
 
 router.get("/", (req, res) => { //  ROUTER ESA BANE GA /form/
-    res.sendFile(path.join(process.cwd(), "views", "form.html"));
+    res.render("form", { users: req.query.name || "User" }); //  template
 });
 
 router.post("/submit", (req, res) => {
