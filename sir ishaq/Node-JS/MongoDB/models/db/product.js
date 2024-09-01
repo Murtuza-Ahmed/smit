5bc4d5dfa9f521243e553f598a1b166a19cdfd2a
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-09-01 13:07:28 +0500

    class end best practice mongoose and quires

--- sir ishaq/Node-JS/MongoDB/models/db/product.js
+++ sir ishaq/Node-JS/MongoDB/models/db/product.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
+// const productSchema = new mongoose.Schema({
+//     title: String,
+//     price: Number,
+// });
+
+const blogSchema = new mongoose.Schema({
     title: String,
-    price: Number,
+    author: String,
+    body: String,
+    email: { type: String, unique: true },
+    password: { type: String, select: false },
+    comments: [{ body: String, date: Date }],
+    date: { type: Date, default: Date.now },
+    hidden: Boolean,
+    meta: { votes: Number, favs: Number }
 });
 
-const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model("Product", blogSchema);
 
 module.exports = Product;
--- sir ishaq/Node-JS/MongoDB/routes/products.js
+++ sir ishaq/Node-JS/MongoDB/routes/products.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const products = require("../data/products.json");
+const Product = require("../models/db/product");
 
-router.get("/", (req, res) => { 
+router.get("/", (req, res) => {
     res.json(products);
+});
+
+router.post("/", async (req, res) => {
+    const product = new Product({
+        title: req.body.title,
+        price: req.body.price,
+    });
+    const resp = await product.save();
+    res.send(resp);
+});
+
+// router.get("/:title", async (req, res) => {
+//     const resp = await Product.findOne({ title: req.params.title });
+//     res.send(resp);
+// });
+
+router.get("/:id", async (req, res) => {
+    const resp = await Product.findById(req.params.id);
+    res.send(resp);
 });
 
 module.exports = router;
