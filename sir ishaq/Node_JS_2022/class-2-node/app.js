d5b25bcc410afb1871713c0ae46a7037d15e9dfb
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-09-03 00:14:04 +0500

    req problem

--- sir ishaq/Node_JS_2022/class-2-node/app.js
+++ sir ishaq/Node_JS_2022/class-2-node/app.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {   // MIDDLE-WERE
+    console.log(req.url);
+    next();
+});
 
+app.post("/form", (req, res, next) => {
+    res.send(req.url + "check");
+});
+
+app.use("/", (req, res, next) => {   // MIDDLE-WERE
+    res.send(`
+        <form action="/form" method="POST"> 
+            <input name="data"/> 
+            <button>Submit</button> 
+        </form>
+    `);
+});
 
 app.listen(3000);
