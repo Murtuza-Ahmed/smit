38d9d7611eb1cba2092281bbc3d613dad8b12329
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-09-08 20:34:31 +0500

    class stop

--- sir ishaq/Node_JS_2022/class-2-node/app.js
+++ sir ishaq/Node_JS_2022/class-2-node/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const fromRoute = require("./routes/form.js");
+const path = require("path");
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {   // MIDDLE-WERE
     console.log(req.url);
