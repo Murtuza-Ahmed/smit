625f273507ae47aa0d9c495b138369e1be40ee02
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-09-08 20:14:04 +0500

    install body parser

--- sir ishaq/Node_JS_2022/class-2-node/app.js
+++ sir ishaq/Node_JS_2022/class-2-node/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const fromRoute = require("./routes/form.js");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.use((req, res, next) => {   // MIDDLE-WERE
     console.log(req.url);
--- sir ishaq/Node_JS_2022/class-2-node/routes/form.js
+++ sir ishaq/Node_JS_2022/class-2-node/routes/form.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 router.post("/", (req, res, next) => {
     res.send(req.body);
 });
 
 router.get("/", (req, res, next) => {   // MIDDLE-WERE
-    res.send(`
-        <form action="/form" method="POST"> 
-            <input name="data" />
-            <button>Submit</button> 
-        </form>
-    `);
+    res.sendFile(path.join(__dirname, "../", "views", "form.html"));
 });
 
 module.exports = router;
