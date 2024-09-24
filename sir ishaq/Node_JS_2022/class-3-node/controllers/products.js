0c739c1c7c9967c3b4d2379d77338fb2c692f697
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-09-24 09:57:25 +0500

    MVC pattren paractice

--- sir ishaq/Node_JS_2022/class-3-node/controllers/products.js
+++ sir ishaq/Node_JS_2022/class-3-node/controllers/products.js
@@ -1,14 +1,18 @@
-const products = [];
+const Product = require("../model/product");
+
+// const products = [];
 
 exports.getAddProduct = (req, res) => {   // MIDDLE-WERE
     res.render("form", { title: "Add Product Form" });
 };
 
 exports.postAddProduct = (req, res) => {
-    products.push(req.body);
+    const product = new Product(req.body.title);
+    product.save();
     res.send("saved!");
 };
 
 exports.fetchAll = (req, res) => {
+    const products = Product.fetchAll();
     res.render("products", {products});
 };
---
+++ sir ishaq/Node_JS_2022/class-3-node/model/product.js
@@ -0,0 +1,15 @@
+const products = [];
+
+class Product {
+    constructor(title) {
+        this.title = title;
+    }
+    save() {
+        products.push({ title: this.title });
+    }
+    static fetchAll() {
+        return products;
+    }
+};
+
+module.exports = Product;
