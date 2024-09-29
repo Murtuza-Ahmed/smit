a1f697d95192c1d43e9c6af7bb83390226d4f0e9
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-09-29 12:59:00 +0500

    create userjson file and create auth folder and user.js helper function

--- sir ishaq/Node_JS_2022/class-4-node/models/user.js
+++ sir ishaq/Node_JS_2022/class-4-node/models/user.js
@@ -1,7 +1,48 @@
-const path = requier("path")
+const path = requier("path");
+const fs = require("fs");
 
-const userJsonFile = path.join(__dirname, "..", "data", "users.json")
+const userJsonFile = path.join(__dirname, "..", "data", "users.json");
 
+const readFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(userJsonFile, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                const dataString = data.toString();
+                if (dataString) {
+                    resolve(JSON.parse(dataString));
+                } else {
+                    resolve([]);
+                };
+            };
+        });
+    });
+};
 
+const writeFile = (dataToWrite) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(userJsonFile, dataToWrite, (err) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve();
+        });
+    });
+};
 
-const storeAUser = () => { }
+const storeAUser = async (user) => {
+    try {
+        let data = await readFile();
+        if (data) {
+            data.push(user);
+        } else {
+            data = [user];
+        };
+        await writeFile();
+    } catch (err) {
+        console.error(err);
+    };
+};
+
+module.exports = storeAUser;
