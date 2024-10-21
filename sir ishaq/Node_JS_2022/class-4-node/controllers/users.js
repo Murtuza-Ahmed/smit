188d9563fc9443ba59ff6a5ed762ed4f7c31b6b7
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-10-21 23:16:33 +0500

    problem solve and run nodemon

--- sir ishaq/Node_JS_2022/class-4-node/controllers/users.js
+++ sir ishaq/Node_JS_2022/class-4-node/controllers/users.js
@@ -2,26 +2,41 @@ const bcrypt = require("bcrypt");
 const userModel = require("../models/user");
 
 exports.getSignIn = (req, res) => {
-    res.render("/auth/signin");
+    res.render("auth/signin");
 };
 
 exports.getSignUp = (req, res) => {
-    res.render("/auth/signup");
+    res.render("auth/signup");
 };
 
-exports.postSignIn = async (req, res) => { };
+exports.postSignIn = async (req, res) => {
+    try {
+        const userCred = req.body;
+        const user = await userModel.getAUser(userCred.email);
+        const result = await bcrypt.compare(userCred.password, user.password);
+        if (result) {
+            req.session.user = userCred.email;
+            res.send("Login Successful")
+        } else {
+            res.send("Invalid email and Password");
+        }
+    } catch (e) {
+        res.send(e);
+    }
+};
 
 exports.postSignUp = async (req, res) => {
-    try {
-        const user = req.body();
-        const users = await userModel.fetchAll();
-        const matched = users.find((u) => u.email === user.email);
-        if (matched) {
-            return res.send("User Already Exits");
-        }
-        await userModel.storeAUser(user);
-        res.redirect("sign-in");
-    } catch (err) {
-        res.send(err);
-    }
+    const user = req.body;
+    userModel.getAUser(user.email)
+        .then((matched) => {
+            return res.send("User Already Exist");
+        })
+        .catch(async (err) => {
+            try {
+                await userModel.storeAUser(user);
+                res.redirect("signin");
+            } catch (err) {
+                console.log(err);
+            }
+        });
 };
--- sir ishaq/Node_JS_2022/class-4-node/models/user.js
+++ sir ishaq/Node_JS_2022/class-4-node/models/user.js
@@ -1,5 +1,6 @@
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
+const bcrypt = require("bcrypt");
 
 const userJsonFile = path.join(__dirname, "..", "data", "users.json");
 
@@ -21,32 +22,49 @@ const readFile = () => {
 
 const writeFile = (dataToWrite) => {
     return new Promise((resolve, reject) => {
-        fs.writeFile(userJsonFile, JSON.stringify(dataToWrite), (err) => {
+        fs.writeFile(userJsonFile, JSON.stringify(dataToWrite), (err, data) => {
             if (err) {
                 return reject(err);
             }
             resolve();
-        });
+        })
     });
 };
 
 exports.storeAUser = async (user) => {
     try {
         let data = await readFile();
+        user.password = await bcrypt.hash(user.password, 12);
         if (data) {
             data.push(user);
         } else {
             data = [user];
-        };
+        }
         await writeFile(data);
         return true;
-    } catch (err) {
-        console.error(err);
-    };
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+exports.getAUser = async (email) => {
+    try {
+        const users = await readFile();
+        const matched = users.find(u => u.email === email);
+        if (matched) {
+            return matched;
+        } else {
+            throw new Error("User not found!");
+        }
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 exports.fetchAll = async () => {
     try {
         return await readFile();
-    } catch (err) { console.error(err) };
-};
+    } catch (e) {
+        console.log(e);
+    }
+}
