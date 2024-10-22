4bfdd05e6b834b6b049e05947a41b0f57f46375f
murtuza-ahmed <murtuza.programmer@gmail.com>
2024-10-22 23:35:06 +0500

    new file add

---
+++ sir ishaq/Node_JS_2022/class-4-node/controllers/products.js
@@ -0,0 +1,18 @@
+const Product = require("../model/product");
+
+// const products = [];
+
+exports.getAddProduct = (req, res) => {   // MIDDLE-WERE
+    res.render("form", { title: "Add Product Form" });
+};
+
+exports.postAddProduct = (req, res) => {
+    const product = new Product(req.body.title);
+    product.save();
+    res.send("saved!");
+};
+
+exports.fetchAll = (req, res) => {
+    const products = Product.fetchAll();
+    res.render("products", {products});
+};
