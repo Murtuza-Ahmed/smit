6d3e90f266c4f2c7d1560a331d34bb3b39ab3c97
owaisuzair <shaikhowais585@gmail.com>
2024-11-03 21:35:29 +0500

    class-5 end and rest api | envinoment read and practice

--- sir ishaq/Node_JS_2022/class-5-node/app.js
+++ sir ishaq/Node_JS_2022/class-5-node/app.js
@@ -7,12 +7,26 @@ const authRoutes = require("./routes/auth.js")
 const productsRoute = require("./routes/products.js");
 const apiRouter = require("./routes/api.js")
 
-
+// console.log(process.env.SECRET)
 app.use(session({
-    secret: 'keyboard cat',
+    secret: process.env.SECRET,  // secret key Environment  variable, (Environment  variable save in package json file script)
+
     resave: false,
     saveUninitialized: false
 }))
+
+app.use((req, res, next) => {
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "DELETE,GET,POST,PATCH",
+        "Access-Control-Allow-Headers": "Content-type, Authorization"
+    })
+    if (req.method === "OPTIONS") {
+        return res.status(200).json({ message: "ok" });
+    } else {
+        next()
+    }
+})
 
 
 app.use(bodyParser.urlencoded({ // x-www-form-urlencoded
